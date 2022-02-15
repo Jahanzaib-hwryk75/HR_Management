815cdc4157a606b927949c387135bbce0249427d
RizwanBlogger <rykrizwan32@gmail.com>
2022-02-15 16:26:20 +0500

    logo change

---
+++ database/seeders/SUPERADMIN.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class SUPERADMIN extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user=new User();
+        $user->email='admin@gmail.com';
+        $user->password=Hash::make('123456');
+        $user->role='superadmin';
+        $user->save();
+
+
+    }
+}
