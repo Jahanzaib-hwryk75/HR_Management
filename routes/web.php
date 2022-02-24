09e36f8ab16f448225118d72b7baaca372f42124
ahmad3187 <saeedi3187@gmail.com>
2022-02-24 15:49:16 +0500

    Project Assign
    Project Assign Successfully.

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\EmployController;
 use App\Http\Controllers\User\RolesController;
 use App\Http\Controllers\User\FrontController;
 use App\Http\Controllers\ImageController;
+use App\Http\Controllers\ProjectController;
 
 
 /*
@@ -47,9 +48,15 @@ Route::group(['prefix' => 'admin'], function () {
     Route::post('roles/delete/{id}', [RolesController::class, 'rolesdlt'])->name('rolesadd');
     Route::post('/signout', [AuthenticationController::class, 'logout']);
     Route::get('/status', [AuthenticationController::class, 'status']);
+<<<<<<< Updated upstream
     // Leave Mangement
     Route::get('/leave',[EmployController::class,'leave']);
     Route::get('/requestleave',[EmployController::class,'requestleave']);
+=======
+    Route::get('/projectassign', [ProjectController::class, 'projectassign']);
+    Route::get('/assign', [ProjectController::class, 'assign']);
+    Route::post('/assign', [ProjectController::class, 'store']);
+>>>>>>> Stashed changes
     
 });
 });
@@ -58,6 +65,9 @@ Route::group(['prefix' => 'admin'], function () {
 // locale Route
 Route::get('lang/{locale}', [LanguageController::class, 'swap']);
 Route::get('convert-pdf-to-image', [ImageController::class, 'index'])->name('form');
+<<<<<<< Updated upstream
 
 
 
+=======
+>>>>>>> Stashed changes
