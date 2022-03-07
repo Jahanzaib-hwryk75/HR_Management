7653fb5f03451dbaf95e06176cc119844493e983
RizwanBlogger <rykrizwan32@gmail.com>
2022-03-07 22:21:33 +0500

    employee

--- resources/views/employee/addemployee.blade.php
+++ resources/views/employee/addemployee.blade.php
@@ -71,9 +71,117 @@
 									<h3 class="stepper-title">Login Info</h3>
 								</div>
 							</div>
-
 						</div>
 					</div>
+				</div>
+				<div class="d-flex flex-row-fluid flex-center bg-body rounded">
+					<form class="py-20 w-100 w-xl-700px px-9" action="{{ url('/admin/saveemployee')}}" method="post">
+						<div class="current slide-page" data-kt-stepper-element="content">
+							<div class="w-100">
+								<div class="pb-10 pb-lg-15">
+									<!--begin::Title-->
+									<h2 class="fw-bolder d-flex align-items-center text-dark">Basic Info</h2>
+								</div>
+								<div class="fv-row">
+									<div class="row">
+										<div class="fv-row mb-10">
+											<!--begin::Label-->
+											<label class="form-label required">First Name</label>
+											<!--end::Label-->
+											<!--begin::Input-->
+											<input name="firstname" class="form-control form-control-lg form-control-solid" type="text" required />
+											@error('firstname')
+											<div class="alert alert-danger">{{ $message }}</div>
+											@enderror
+											@csrf
+											<!--end::Input-->
+										</div>
+										<div class="fv-row mb-10">
+											<!--begin::Label-->
+											<label class="form-label required">Last Name</label>
+											<!--end::Label-->
+											<!--begin::Input-->
+											<input name="lastname" class="form-control form-control-lg form-control-solid" type="text" /> @error('lastname')
+											<div class="alert alert-danger">{{ $message }}</div>
+											@enderror
+											<!--end::Input-->
+										</div>
+										<div class="fv-row mb-10">
+											<!--begin::Label-->
+											<label class="form-label required">Email</label>
+											<!--end::Label-->
+											<!--begin::Input-->
+											<input name="email" class="form-control form-control-lg form-control-solid" type="email" /> @error('email')
+											<div class="alert alert-danger">{{ $message }}</div>
+											@enderror
+											<!--end::Input-->
+										</div>
+										<div class="fv-row mb-10">
+											<!--begin::Label-->
+											<label class="form-label required">Phone No</label>
+											<!--end::Label-->
+											<!--begin::Input-->
+											<input name="phonenumber" class="form-control form-control-lg form-control-solid" type="text" /> @error('phonenumber')
+											<div class="alert alert-danger">{{ $message }}</div>
+											@enderror
+											<!--end::Input-->
+										</div>
+										<div class="fv-row mb-10">
+											<!--begin::Label-->
+											<label class="form-label required">Alternative Phone</label>
+											<!--end::Label-->
+											<!--begin::Input-->
+											<input name="alternativephone" class="form-control form-control-lg form-control-solid" type="text" /> @error('alternativephone')
+											<div class="alert alert-danger">{{ $message }}</div>
+											@enderror
+											<!--end::Input-->
+										</div>
+										<div class="fv-row mb-10">
+											<!--begin::Label-->
+											<label class="form-label required">Country</label>
+											<!--end::Label-->
+											<!--begin::Input-->
+											<select name="selectcountry" class="form-select form-select-lg form-select-solid" data-control="select2" data-placeholder="Select..." data-allow-clear="true" data-hide-search="true">
+												<option></option>
+												<option>Pakistan</option>
+												<option>USA</option>
+											</select>
+											@error('selectcountry')
+											<div class="alert alert-danger">{{ $message }}</div>
+											@enderror
+											<!--end::Input-->
+										</div>
+										<div class="fv-row mb-10">
+											<!--begin::Label-->
+											<label class="form-label required">Cite</label>
+											<!--end::Label-->
+											<!--begin::Input-->
+											<input name="city" class="form-control form-control-lg form-control-solid" type="text" /> @error('city')
+											<div class="alert alert-danger">{{ $message }}</div>
+											@enderror
+											<!--end::Input-->
+										</div>
+										<div class="fv-row mb-10">
+											<!--begin::Label-->
+											<label class="form-label required">Zip Code</label>
+											<!--end::Label-->
+											<!--begin::Input-->
+											<input name="zipcode" class="form-control form-control-lg form-control-solid" type="text" /> @error('zipcode')
+											<div class="alert alert-danger">{{ $message }}</div>
+											@enderror
+											<!--end::Input-->
+										</div>
+										<div class="fv-row mb-10">
+											<button type="button" class="btn btn-lg btn-primary firstNext" data-kt-stepper-action="next">Continue
+											</button>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						
+					</form>
+					
 				</div>
 				
 			</div>
@@ -81,7 +189,7 @@
 	</div>
 
 	@endsection
-
+	<script src="{{ asset('scripts/script.js') }}"></script>
 	<script src="{{ asset('newadmin/assets/css/style.bundle.js') }}"></script>
 	<script src="{{ asset('newadmin/assets/css/style.bundle.js.map') }}"></script>
 	<script src="{{ asset('newadmin/assets/js/scripts.bundle.js') }}"></script>
