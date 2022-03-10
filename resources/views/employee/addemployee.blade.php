445b9297954fe94454ad39e9d044df17b612de6e
RizwanBlogger <rykrizwan32@gmail.com>
2022-03-10 16:47:52 +0500

    addemployeeformcreate

--- resources/views/employee/addemployee.blade.php
+++ resources/views/employee/addemployee.blade.php
@@ -3,52 +3,67 @@
 <form class="">
 
 	<div class="row">
-	
-        <h4 class="card-title">Basic Info</h4>
-    
-		<div class="col-md-4 col-12">
+		<h4 class="card-title">Basic Info</h4>
+		<div class="col-md-6 col-12">
 			<div role="group" class="form-group" id="__BVID__1886"><label for="username" class="d-block" id="__BVID__1886__BV_label_">First Name</label>
-				<div><input id="username" type="text" class="form-control">
+				<div><input type="text" id="firstname" name="firstname" type="text" class="form-control">
 				</div>
 			</div>
 		</div>
-		<div class="col-md-4 col-12">
+		<div class="col-md-6 col-12">
 			<div role="group" class="form-group" id="__BVID__1888"><label for="full-name" class="d-block" id="__BVID__1888__BV_label_">Last Name</label>
-				<div><input id="full-name" type="text" class="form-control">
+				<div><input type="text" name="lastname" id="lastname" type="text" class="form-control">
 				</div>
 			</div>
 		</div>
-		<div class="col-md-4 col-12">
+		<!-- <div class="col-md-4 col-12">
 			<div role="group" class="form-group" id="__BVID__1890"><label for="email" class="d-block" id="__BVID__1890__BV_label_">Email Address</label>
 				<div><input id="email" type="email" class="form-control">
 				</div>
 			</div>
+		</div> -->
+	</div>
+	<div class="row">
+		<div class="col-md-4 col-12">
+			<div role="group" class="form-group" id="__BVID__1890"><label for="email" class="d-block" id="__BVID__1890__BV_label_">Email Address</label>
+				<div><input name="email" id="email" type="email" class="form-control">
+				</div>
+			</div>
 		</div>
-</div>
- <div class="row">
 		<div class="col-md-4 col-12">
 			<div role="group" class="form-group" id="__BVID__1886"><label for="username" class="d-block" id="__BVID__1886__BV_label_">Phone No</label>
-				<div><input id="username" type="text" class="form-control">
+				<div><input id="phonenumber" name="phonenumber" type="text" class="form-control">
 				</div>
 			</div>
 		</div>
 		<div class="col-md-4 col-12">
 			<div role="group" class="form-group" id="__BVID__1888"><label for="full-name" class="d-block" id="__BVID__1888__BV_label_">Alternative Phone</label>
-				<div><input id="full-name" type="text" class="form-control">
+				<div><input id="alternativephone" name="alternativephone" type="text" class="form-control">
 				</div>
 			</div>
 		</div>
-		<div class="col-md-4 col-12">
+		<!-- <div class="col-md-4 col-12">
 			<div role="group" class="form-group" id="__BVID__1890"><label for="email" class="d-block" id="__BVID__1890__BV_label_">Country</label>
 				<div><input id="email" type="email" class="form-control">
 				</div>
 			</div>
+		</div> -->
+	</div>
+	<div class="row">
+		<div class="col-md-4 col-12">
+			<div role="group" class="form-group" id="__BVID__1890"><label for="email" class="d-block" id="__BVID__1890__BV_label_">Country</label>
+				<div> <select class="form-select" id="selectcountry" name="selectcountry">
+                    <option></option>
+                      <option></option>
+                      <option></option>
+                      <option></option>
+                    </select> 
+				</div>
+			</div>
 		</div>
-</div>
-<div class="row">
 		<div class="col-md-4 col-12">
 			<div role="group" class="form-group" id="__BVID__1886"><label for="username" class="d-block" id="__BVID__1886__BV_label_">City</label>
-				<div><input id="username" type="text" class="form-control">
+				<div><input id="city" name="city" type="text" class="form-control">
 				</div>
 			</div>
 		</div>
@@ -60,42 +75,79 @@
 		</div> -->
 		<div class="col-md-4 col-12">
 			<div role="group" class="form-group" id="__BVID__1890"><label for="email" class="d-block" id="__BVID__1890__BV_label_">Zip Code</label>
-				<div><input id="email" type="email" class="form-control">
+				<div><input id="zipcode" type="text" name="zipcode" class="form-control">
 				</div>
 			</div>
 		</div>
-</div>
-<div class="row">
-<h4 class="card-title my-2">Positional Information</h4>
+	</div>
+
+
+	<div class="row">
+		<h4 class="card-title my-2">Positional Information</h4>
 		<div class="col-md-4 col-12">
 			<div role="group" class="form-group" id="__BVID__1886"><label for="username" class="d-block" id="__BVID__1886__BV_label_">Division</label>
-				<div><input id="username" type="text" class="form-control">
+				<div><select class="form-select" id="division" name="division">
+                    <option></option>
+                      <option></option>
+                      <option></option>
+                      <option></option>
+                    </select> 
 				</div>
 			</div>
 		</div>
 		<div class="col-md-4 col-12">
 			<div role="group" class="form-group" id="__BVID__1888"><label for="full-name" class="d-block" id="__BVID__1888__BV_label_">Position</label>
-				<div><input id="full-name" type="text" class="form-control">
+				<div><select class="form-select" id="positionname" name="positionname">
+                    <option></option>
+                      <option></option>
+                      <option></option>
+                      <option></option>
+                    </select> 
 				</div>
 			</div>
 		</div>
 		<div class="col-md-4 col-12">
-			<div role="group" class="form-group" id="__BVID__1890"><label for="email" class="d-block" id="__BVID__1890__BV_label_">Duty Type</label>
+			<div role="group" class="form-group" id="__BVID__1890"><label for="dutytype" class="d-block" id="__BVID__1890__BV_label_">Duty Type</label>
+				<div><select class="form-select" id="dutytype" name="dutytype">
+                    <option></option>
+                      <option></option>
+                      <option></option>
+                      <option></option>
+                    </select> 
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-4 col-12">
+			<div role="group" class="form-group" id="__BVID__1886"><label for="hiredate" class="d-block" id="__BVID__1886__BV_label_">Hire Date</label>
+				<div><input id="hiredate" name="hiredate" type="date" class="form-control">
+				</div>
+			</div>
+		</div>
+		<div class="col-md-4 col-12">
+			<div role="group" class="form-group" id="__BVID__1888"><label for="fterminationdate" class="d-block" id="__BVID__1888__BV_label_">Termination Date</label>
+				<div><input id="terminationdate" name="terminationdate" type="date" class="form-control">
+				</div>
+			</div>
+		</div>
+		<div class="col-md-4 col-12">
+			<div role="group" class="form-group" id="__BVID__1888"><label for="rehiredate" class="d-block" id="__BVID__1888__BV_label_">Re Hire Date</label>
+				<div><input id="rehiredate" name="rehiredate" type="date" class="form-control">
+				</div>
+			</div>
+		</div>
+		<!-- <div class="col-md-4 col-12">
+			<div role="group" class="form-group" id="__BVID__1890"><label for="email" class="d-block" id="__BVID__1890__BV_label_">Termination Reason</label>
 				<div><input id="email" type="email" class="form-control">
 				</div>
 			</div>
-		</div>
-</div>
-<div class="row">
+		</div> -->
+	</div>
+	<div class="row">
 		<div class="col-md-4 col-12">
-			<div role="group" class="form-group" id="__BVID__1886"><label for="username" class="d-block" id="__BVID__1886__BV_label_">Hire Date</label>
+			<div role="group" class="form-group" id="__BVID__1886"><label for="username" class="d-block" id="__BVID__1886__BV_label_">Voluntary Termination</label>
 				<div><input id="username" type="text" class="form-control">
-				</div>
-			</div>
-		</div>
-		<div class="col-md-4 col-12">
-			<div role="group" class="form-group" id="__BVID__1888"><label for="full-name" class="d-block" id="__BVID__1888__BV_label_">Termination Date</label>
-				<div><input id="full-name" type="text" class="form-control">
 				</div>
 			</div>
 		</div>
@@ -105,28 +157,20 @@
 				</div>
 			</div>
 		</div>
-</div>
-<div class="row">
-		<div class="col-md-4 col-12">
-			<div role="group" class="form-group" id="__BVID__1886"><label for="username" class="d-block" id="__BVID__1886__BV_label_">Voluntary Termination</label>
-				<div><input id="username" type="text" class="form-control">
-				</div>
-			</div>
-		</div>
-		<div class="col-md-4 col-12">
+		<!-- <div class="col-md-4 col-12">
 			<div role="group" class="form-group" id="__BVID__1888"><label for="full-name" class="d-block" id="__BVID__1888__BV_label_">Re Hire Date</label>
 				<div><input id="full-name" type="text" class="form-control">
 				</div>
 			</div>
-		</div>
+		</div> -->
 		<div class="col-md-4 col-12">
 			<div role="group" class="form-group" id="__BVID__1890"><label for="email" class="d-block" id="__BVID__1890__BV_label_">Rate Type</label>
 				<div><input id="email" type="email" class="form-control">
 				</div>
 			</div>
 		</div>
-</div>
-<div class="row">
+	</div>
+	<div class="row">
 		<div class="col-md-4 col-12">
 			<div role="group" class="form-group" id="__BVID__1886"><label for="username" class="d-block" id="__BVID__1886__BV_label_">Rate</label>
 				<div><input id="username" type="text" class="form-control">
@@ -145,8 +189,8 @@
 				</div>
 			</div>
 		</div>
-</div>
-<div class="row">
+	</div>
+	<div class="row">
 		<div class="col-md-4 col-12">
 			<div role="group" class="form-group" id="__BVID__1886"><label for="username" class="d-block" id="__BVID__1886__BV_label_">Home Department</label>
 				<div><input id="username" type="text" class="form-control">
@@ -165,8 +209,15 @@
 				</div>
 			</div>
 		</div> -->
-</div>
-<!--<div class="row">
+	</div>
+	<div class="row">
+		<h4 class="card-title my-2">Biographical Info</h4>
+		<div class="col-md-4 col-12">
+			<div role="group" class="form-group" id="__BVID__1890"><label for="email" class="d-block" id="__BVID__1890__BV_label_">Date of Birth*</label>
+				<div><input id="email" type="email" class="form-control">
+				</div>
+			</div>
+		</div>
 		<div class="col-md-4 col-12">
 			<div role="group" class="form-group" id="__BVID__1886"><label for="username" class="d-block" id="__BVID__1886__BV_label_">Gender</label>
 				<div><input id="username" type="text" class="form-control">
@@ -179,17 +230,23 @@
 				</div>
 			</div>
 		</div>
+		<!-- <div class="col-md-4 col-12">
+			<div role="group" class="form-group" id="__BVID__1890"><label for="email" class="d-block" id="__BVID__1890__BV_label_">Work in State</label>
+				<div><input id="email" type="email" class="form-control">
+				</div>
+			</div>
+		</div> -->
+	</div>
+	<div class="row">
 		<div class="col-md-4 col-12">
 			<div role="group" class="form-group" id="__BVID__1890"><label for="email" class="d-block" id="__BVID__1890__BV_label_">Work in State</label>
 				<div><input id="email" type="email" class="form-control">
 				</div>
 			</div>
 		</div>
-</div>
-<div class="row">
 		<div class="col-md-4 col-12">
-			<div role="group" class="form-group" id="__BVID__1886"><label for="username" class="d-block" id="__BVID__1886__BV_label_"> 
-              Live in State</label>
+			<div role="group" class="form-group" id="__BVID__1886"><label for="username" class="d-block" id="__BVID__1886__BV_label_">
+					Live in State</label>
 				<div><input id="username" type="text" class="form-control">
 				</div>
 			</div>
@@ -201,14 +258,15 @@
 			</div>
 		</div>
 		<div class="col-md-4 col-12">
-			<div role="group" class="form-group" id="__BVID__1890"><label for="email" class="d-block" id="__BVID__1890__BV_label_"> 
-             Photograph</label>
+			<div role="group" class="form-group" id="__BVID__1890"><label for="email" class="d-block" id="__BVID__1890__BV_label_">
+					Photograph</label>
 				<div><input id="email" type="email" class="form-control">
 				</div>
 			</div>
 		</div>
-</div>
-<div class="row">
+	</div>
+	<div class="row">
+		<h4 class="card-title my-2">Additional Address</h4>
 		<div class="col-md-4 col-12">
 			<div role="group" class="form-group" id="__BVID__1886"><label for="username" class="d-block" id="__BVID__1886__BV_label_">Home Email</label>
 				<div><input id="username" type="text" class="form-control">
@@ -227,15 +285,15 @@
 				</div>
 			</div>
 		</div>
-</div>
-<div class="row">
+	</div>
+	<div class="row">
 		<div class="col-md-4 col-12">
 			<div role="group" class="form-group" id="__BVID__1886"><label for="username" class="d-block" id="__BVID__1886__BV_label_">Cell Phone</label>
 				<div><input id="username" type="text" class="form-control">
 				</div>
 			</div>
 		</div>
-		<div class="col-md-4 col-12">
+		<!-- <div class="col-md-4 col-12">
 			<div role="group" class="form-group" id="__BVID__1888"><label for="full-name" class="d-block" id="__BVID__1888__BV_label_">Emergency Contact</label>
 				<div><input id="full-name" type="text" class="form-control">
 				</div>
@@ -246,9 +304,10 @@
 				<div><input id="email" type="email" class="form-control">
 				</div>
 			</div>
-		</div>
-</div>
-<div class="row">
+		</div> -->
+	</div>
+	<div class="row">
+		<h4 class="card-title my-2">Emergency Contact</h4>
 		<div class="col-md-4 col-12">
 			<div role="group" class="form-group" id="__BVID__1886"><label for="username" class="d-block" id="__BVID__1886__BV_label_">Emergency Contact Relation</label>
 				<div><input id="username" type="text" class="form-control">
@@ -267,8 +326,23 @@
 				</div>
 			</div>
 		</div>
-</div>
-<div class="row">
+	</div>
+	<div class="row">
+		<div class="col-md-4 col-12">
+			<div role="group" class="form-group" id="__BVID__1888"><label for="full-name" class="d-block" id="__BVID__1888__BV_label_">Emergency Contact</label>
+				<div><input id="full-name" type="text" class="form-control">
+				</div>
+			</div>
+		</div>
+		<div class="col-md-4 col-12">
+			<div role="group" class="form-group" id="__BVID__1890"><label for="email" class="d-block" id="__BVID__1890__BV_label_">Emergency Home Phone</label>
+				<div><input id="email" type="email" class="form-control">
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<h4 class="card-title my-2">Login Info</h4>
 		<div class="col-md-4 col-12">
 			<div role="group" class="form-group" id="__BVID__1886"><label for="username" class="d-block" id="__BVID__1886__BV_label_">User Email</label>
 				<div><input id="username" type="text" class="form-control">
@@ -281,13 +355,13 @@
 				</div>
 			</div>
 		</div>
-		<div class="col-md-4 col-12">
-			<div role="group" class="form-group" id="__BVID__1890"><label for="email" class="d-block" id="__BVID__1890__BV_label_"></label>
-				<div>
-				</div>
-			</div>
-		</div>
-</div> -->
+
+	</div>
+	<div class="col-sm-8 offset-sm-5 my-2">
+		<button type="submit" class="btn btn-primary me-1">Submit</button>
+	</div>
+
+
+	</div>
 </form>
 @endsection
-
