e4f8fbae288f8122cfeacb3631b76be9cd154e4c
RizwanBlogger <rykrizwan32@gmail.com>
2022-03-14 16:39:07 +0500

    create payroll page

--- app/Http/Controllers/PayrollController.php
+++ app/Http/Controllers/PayrollController.php
@@ -6,5 +6,19 @@ use Illuminate\Http\Request;
 
 class PayrollController extends Controller
 {
-    //
+   public function salarytypesetup(){
+       return view('payroll.salarytypesetup');
+   }
+   public function addsalarybenfit(){
+       return view('payroll.addsalarybenfit');
+   }
+   public function managesalarybenefits(){
+    return view('payroll.managesalarybenefits');
+}
+public function createsetup(){
+    return view('payroll.createsetup');
+}
+public function managesalarysetup(){
+    return view('payroll.managesalarysetup');
+}
 }
